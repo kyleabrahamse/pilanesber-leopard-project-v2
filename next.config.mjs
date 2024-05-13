42038f0ac78d2b7a666df96e75a45d44d0d57bf5
Kyle Abrahamse <kyleabrahamse@outlook.com>
2024-05-13 13:23:32 +0100

    adding Cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["firebasestorage.googleapis.com", "www.spongebobshop.com", "m.media-amazon.com"],
+    domains: ["firebasestorage.googleapis.com", "www.spongebobshop.com", "m.media-amazon.com", "https://pilanesberg-leopard-project.netlify.app/Sightings"],
   },
 };
 
