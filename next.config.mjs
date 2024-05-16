6544226f0a5c4412af4e6238204c8990cc9f3749
Kyle Abrahamse <kyleabrahamse@outlook.com>
2024-05-16 19:24:10 +0100

    added headers for cors settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["firebasestorage.googleapis.com", "www.spongebobshop.com", "m.media-amazon.com", "https://pilanesberg-leopard-project.netlify.app/Sightings"],
+    domains: [
+      "firebasestorage.googleapis.com",
+      "www.spongebobshop.com",
+      "m.media-amazon.com",
+      "https://pilanesberg-leopard-project.netlify.app/Sightings",
+    ],
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:gs://leopard-project.appspot.com/images",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://pilanesberg-leopard-project.netlify.app/",
+          }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
   },
 };
 
