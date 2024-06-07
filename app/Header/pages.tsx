68eb954252a5d868daf0059537c76e8b979c2b6a
Kyle Abrahamse <kyleabrahamse@outlook.com>
2024-06-07 21:44:36 +0100

    mobile menu working with transition

--- app/Header/pages.tsx
+++ app/Header/pages.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Button from "../components/Button";
@@ -15,22 +15,60 @@ export default function Header() {
   };
 
   return (
-    <div className="flex items-center justify-between bg-cornsilk ">
-      <div
-        className={`flex ${openMobileMenu && "flex-col"} items-center font-medium`}
-      >
-        <Link href="/">
-          <Image
-            className="mr-5 w-32"
-            src="/images/logo.png"
-            alt="logo"
-            width={500}
-            height={500}
-          />
-        </Link>
-        <ul
-          className={`gap-6 text-2xl xl:flex ${openMobileMenu ? "flex flex-col pb-5 pl-5 pt-10" : "hidden"}`}
+    <div>
+      <div className="flex items-center justify-between bg-cornsilk">
+        <div
+          className={`flex ${openMobileMenu && "flex-col"} items-center font-medium`}
         >
+          <Link href="/">
+            <Image
+              className="mr-5 w-32"
+              src="/images/logo.png"
+              alt="logo"
+              width={500}
+              height={500}
+            />
+          </Link>
+          <ul className="hidden gap-6 text-2xl xl:flex">
+            <Link href="/About">
+              <li>About</li>
+            </Link>
+            <Link href="/Tours">
+              <li>Tours</li>
+            </Link>
+            <Link href="/Gallery">
+              <li>Gallery</li>
+            </Link>
+            <Link href="/LeopardProfiles">
+              <li>Leopard Profiles</li>
+            </Link>
+            <Link href="/Sightings">
+              <li>Sightings Board</li>
+            </Link>
+            <Link href="/Conservation">
+              <li>Conservation</li>
+            </Link>
+          </ul>
+        </div>
+        <div className="hidden xl:flex">
+          <Link href="/Contact">
+            <Button text="Contact" buttonType="white" />
+          </Link>
+          <Link href="/Conservation">
+            <Button text="Support" buttonType="black" />
+          </Link>
+        </div>
+        <div className="pr-10 xl:hidden" onClick={openMenu}>
+          <MenuIcon className={openMobileMenu ? "hidden" : ""} />
+          <CloseIcon className={openMobileMenu ? "" : "hidden"} />
+        </div>
+      </div>
+      <div
+        className={`mobile-menu bg-cornsilk transition-all duration-500 ease-in-out overflow-hidden ${
+          openMobileMenu ? "max-h-[500px] visible" : "max-h-0 invisible"
+        }`}
+      >
+        <ul className="flex flex-col gap-6 text-2xl p-6">
           <Link href="/About">
             <li>About</li>
           </Link>
@@ -50,14 +88,6 @@ export default function Header() {
             <li>Conservation</li>
           </Link>
         </ul>
-      </div>
-      <div className="hidden xl:flex ">
-        <Link href="/Contact">
-          <Button text="Contact" buttonType="white" />
-        </Link>
-        <Link href="/Conservation">
-          <Button text="Support" buttonType="black" />
-        </Link>
       </div>
     </div>
   );
